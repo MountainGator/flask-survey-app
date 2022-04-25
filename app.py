8733d85a06ec155d76e79c976edc52fccc8c342c
MountainGator <luketaylorsanders@gmail.com>
2022-04-25 06:49:30 -0500

    try to figure out naming issue

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
+from crypt import methods
 from flask import Flask, request, render_template, redirect, flash
 from random import randint, choice, sample
-from flask_debugtoolbar import DebugToolbarExtension
+# from flask_debugtoolbar import DebugToolbarExtension
 from surveys import Survey, satisfaction_survey
 
 app = Flask(__name__)
-debug = DebugToolbarExtension(app)
+# debug = DebugToolbarExtension(app)
 
 responses = list()
 
@@ -14,4 +15,12 @@ def home_route():
 
 @app.route('/survey')
 def take_survey():
-    return render_template('survey', survey=satisfaction_survey)
+    count = 0
+    return render_template('survey.html', survey=satisfaction_survey, count=count)
+
+@app.route('/survey/thank-you', methods=["POST"])
+def thank_you():
+    for answer in request.form:
+        responses.append(answer)
+    print(responses)
+    return render_template('thank-you.html')
